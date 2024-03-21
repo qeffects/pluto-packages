f26017698b0340333a2f3342a2e86ca205b96999
qfx <qfluxstudio@gmail.com>
2024-03-21 16:19:15 +0200

    update manifest files

--- start.mjs
+++ start.mjs
@@ -3,27 +3,34 @@ import { createHash } from "node:crypto";
 import fs from "node:fs/promises";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import simpleGit from "simple-git";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const packages = await fs.readdir("./packages");
 
+
 const hashes = [];
 
 for (let index = 0; index < packages.length; index++) {
     const val = packages[index];
-    let jsonModule = await fs.readFile("./packages/"+val+"/module/module.json");
-    let module = JSON.parse(jsonModule);
+    const jsonModule = await fs.readFile("./packages/"+val+"/module/module.json");
+    const module = JSON.parse(jsonModule);
     
-    let zipFile = await fs.readFile(`./${val}.tar.gz`);
-    let hash = createHash('sha256').update(zipFile, 'binary').digest('hex');
+    const zipFile = await fs.readFile(`./${val}.tar.gz`);
+    const hash = createHash('sha256').update(zipFile, 'binary').digest('hex');
+
+    const git = simpleGit("./packages/"+val+"/module/");
+
+    module.tags = JSON.stringify(await git.tags());
+    module.latestCommit = (await git.log()).latest.hash;
 
     module.hash = hash;
     module.size = zipFile.byteLength;
 
     hashes.push(hash);
 
-    await fs.writeFile(`./packages/${val}/manifest.json`, JSON.stringify(module), "utf8");
+    await fs.writeFile(`./packages/${val}/manifest.json`, JSON.stringify(module, null, 4), "utf8");
     console.log(`Hashed ${val} module: ${hash} , size: ${module.size}`);
 }
